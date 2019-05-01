5b8ee50f863ddde9c31d97678f4c64aa95f55f4a
shash222 <shash222@yahoo.com>
2019-04-30 21:51:50 -0400

    Creates array out of input data, still need to finish create and parsing array for returned data from server to client

---
+++ WTF.h
@@ -0,0 +1,11 @@
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <time.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <pthread.h>
