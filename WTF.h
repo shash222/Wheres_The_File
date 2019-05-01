a8d8634ebd76e0ffae007ba47336eb6fd447b4f1
Usama Sajid <usanasm@yahoo.com>
2019-05-01 14:37:47 -0400

    fileHash adder and deletion from client/.Manifest

--- WTF.h
+++ WTF.h
@@ -9,3 +9,30 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <pthread.h>
+
+
+/*
+ * adds sha256 hashsum of file under client/.Manifest file 
+ * 
+ * */
+void addFileHash(char * filename)
+{
+  char cmd[50];
+  sprintf(cmd, "sha256sum %s >> client/.Manifest", filename);
+  system(cmd);
+}
+
+/*
+ * delets file and current hashsum from client/.Manifest
+ *  -- to be used in updates / commits
+ *
+ * */
+
+void delFileHash(char * filename)
+{
+  char cmd[50];
+  sprintf(cmd, "sed -i '\\:%s:d' client/.Manifest", filename);
+  cmd[strlen(cmd)] = '\0';
+  system(cmd);
+}
+
